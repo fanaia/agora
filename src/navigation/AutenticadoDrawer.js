9b3c95b5779163ae0a710542377bdd430426a27c
fabio <fabio@pdvseven.com.br>
2023-07-17 00:28:57 -0300

    adicionado componente para fazer leitura de QRCode

--- src/navigation/AutenticadoDrawer.js
+++ src/navigation/AutenticadoDrawer.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {createDrawerNavigator} from '@react-navigation/drawer';
 
+import ScanScreen from '../screens/testes/ScanScreen';
 import InventarioStack from './InventarioStack';
 import ComprasScreen from '../screens/compras/ComprasScreen';
 import TransparenciaScreen from '../screens/transparencia/TransparenciaScreen';
@@ -11,6 +12,7 @@ const Drawer = createDrawerNavigator();
 const AutenticadoDrawer = () => {
   return (
     <Drawer.Navigator initialRouteName="Inventario">
+      {/* <Drawer.Screen name="ScanScreen" component={ScanScreen} /> */}
       <Drawer.Screen name="InventarioStack" component={InventarioStack} />
       <Drawer.Screen name="ComprasScreen" component={ComprasScreen} />
       <Drawer.Screen
--- src/screens/testes/ScanScreen.js
+++ src/screens/testes/ScanScreen.js
@@ -3,26 +3,21 @@ import {StyleSheet, Text, View, Dimensions} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
 
-const CapturaQRCode = ({onQRCodeRead}) => {
+const ScanScreen = () => {
   const [scannedValue, setScannedValue] = useState('');
   const cameraRef = useRef(null);
 
   const handleBarCodeRead = event => {
     if (!scannedValue) {
       setScannedValue(event.data);
-      onQRCodeRead(event.data);
     }
-  };
-
-  const handleScanAgain = () => {
-    setScannedValue(''); // Limpa o valor do código lido para que possa ser lido novamente
   };
 
   const drawRectangleOnCamera = () => {
     const windowWidth = Dimensions.get('window').width;
-    // const windowHeight = Dimensions.get('window').height;
+    const windowHeight = Dimensions.get('window').height;
     const squareSize = 250;
-    const squarePositionTop = squareSize / 2;
+    const squarePositionTop = (windowHeight - squareSize) / 2;
     const squarePositionLeft = (windowWidth - squareSize) / 2;
 
     return (
@@ -47,19 +42,11 @@ const CapturaQRCode = ({onQRCodeRead}) => {
         style={styles.camera}
         type={RNCamera.Constants.Type.back}
         flashMode={RNCamera.Constants.FlashMode.off}
-        onBarCodeRead={handleBarCodeRead}
-        captureAudio={false}>
+        onBarCodeRead={handleBarCodeRead}>
         {drawRectangleOnCamera()}
       </RNCamera>
       {scannedValue ? (
-        <View>
-          <Text style={styles.scanAgainText} onPress={handleScanAgain}>
-            Clique para escanear novamente
-          </Text>
-          <Text style={styles.scannedValueText}>
-            Valor lido: {scannedValue}
-          </Text>
-        </View>
+        <Text style={styles.scannedValueText}>{scannedValue}</Text>
       ) : null}
     </View>
   );
@@ -87,15 +74,6 @@ const styles = StyleSheet.create({
     position: 'absolute',
     top: 20,
   },
-  scanAgainText: {
-    backgroundColor: 'rgba(0, 0, 0, 0.5)',
-    color: 'white',
-    fontSize: 16,
-    padding: 10,
-    alignSelf: 'center',
-    position: 'absolute',
-    bottom: 20,
-  },
 });
 
-export default CapturaQRCode;
+export default ScanScreen;
