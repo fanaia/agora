c8e824cbe47d4521c5289ec193c31634f90e677a
fabio <fabio@pdvseven.com.br>
2023-07-18 12:23:29 -0300

    configurando o uso do .env

--- src/screens/compras/ComprasScreen.js
+++ src/screens/compras/ComprasScreen.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Text} from 'react-native';
+import {API_URL, API_TOKEN} from '@env';
 
 import Screen from '../../components/Screen';
 import Section from '../../components/Section';
@@ -6,7 +8,10 @@ import Section from '../../components/Section';
 const ComprasScreen = () => {
   return (
     <Screen>
-      <Section title="Compras" />
+      <Section title="Compras">
+        <Text>API_URL: {API_URL}</Text>
+        <Text>API_TOKEN : {API_TOKEN}</Text>
+      </Section>
     </Screen>
   );
 };
